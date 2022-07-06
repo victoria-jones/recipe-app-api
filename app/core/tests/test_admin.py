debbf1e8b86c2aee88325a9a4e7456041f930be6
Victoria <victoria.j.art@gmail.com>
2022-07-06 16:05:26 -0700

    admin add user stuff

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -21,13 +21,27 @@ class AdminSiteTests(TestCase):
         self.user = get_user_model().objects.create_user(
             email='user@example.com',
             password='testpass123',
-            name='test user',
+            name='test user'
         )
 
-    def set_users_list(self):
+    def test_users_list(self):
         """Test that users are listed on page."""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """test the edit user page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """test the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
