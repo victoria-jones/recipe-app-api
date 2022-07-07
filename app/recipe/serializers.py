af55a9f77192834b7c2263e2b3706e7a68a99673
Victoria <victoria.j.art@gmail.com>
2022-07-07 15:15:07 -0700

    added recipe api

---
+++ app/recipe/serializers.py
@@ -0,0 +1,15 @@
+"""
+serializers for recipe apis
+"""
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """serializer for recipes"""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
---
+++ app/recipe/urls.py
@@ -0,0 +1,21 @@
+"""
+url mappings for the recipe app
+"""
+from django.urls import (
+    path,
+    include,
+)
+
+from rest_framework.routers import DefaultRouter
+
+from recipe import views
+
+
+router = DefaultRouter()
+router.register('recipes', views.RecipeViewSet)
+
+app_name = 'recipe'
+
+urlpatters = [
+    path('', include(router.urls)),
+]
